82a82383ddd5a6a1a74cd0bdc5108fb9e547b6ce
csyun <csyun@creativelab-inc.com>
2023-03-27 09:41:24 +0900

    .

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,7 +1,8 @@
+import { useQuery } from 'react-query';
+
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
 // async function getTreatments(): Promise<Treatment[]> {
@@ -9,7 +10,14 @@ import { useCustomToast } from '../../app/hooks/useCustomToast';
 //   return data;
 // }
 
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get('/treatments');
+  return data;
+}
+
 export function useTreatments(): Treatment[] {
   // TODO: get data from server via useQuery
-  return [];
+  const fallback = [];
+  const { data = fallback } = useQuery([queryKeys.treatments], getTreatments);
+  return data;
 }
